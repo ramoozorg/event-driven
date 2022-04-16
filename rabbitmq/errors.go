95846c05865c29bbf36bda6f7edaaa6c6c762cd9
Javad <ja7ad@live.com>
2022-04-16 16:27:36 +0430

    add some error for errors list

--- rabbitmq/errors.go
+++ rabbitmq/errors.go
@@ -3,9 +3,12 @@ package rabbitmq
 import "errors"
 
 var (
-	SERVICE_NAME_ERROR       = errors.New("service name is empty")
-	URI_ADDRESS_ERROR        = errors.New("uri address is invalid, please enter amqp://guest:guest@localhost:5672 for example")
-	ROUTING_KEYS_EMPTY_ERROR = errors.New("routing keys is empty")
-	CONNECTION_CLOSED_ERROR  = errors.New("rabbitMQ connection closed, try to reconnect")
-	NIL_CCONECTION_ERROR     = errors.New("nil rabbitmq connection")
+	SERVICE_NAME_ERROR            = errors.New("service name is empty")
+	URI_ADDRESS_ERROR             = errors.New("uri address is invalid, please enter amqp://guest:guest@localhost:5672 for example")
+	ROUTING_KEYS_EMPTY_ERROR      = errors.New("routing keys is empty")
+	CONNECTION_CLOSED_ERROR       = errors.New("rabbitMQ connection closed, try to reconnect")
+	NIL_CCONECTION_ERROR          = errors.New("nil rabbitmq connection")
+	EXCHANGE_ALREADY_EXISTS_ERROR = errors.New("exchange already declared")
+	QUEUE_ALREADY_EXISTS_ERROR    = errors.New("queue already declared")
+	EXHCNAGE_NOT_FOUND_ERROR      = errors.New("exchange not declare")
 )
