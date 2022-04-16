bc8f33a1536c42772955289eb940a38734194155
Javad <ja7ad@live.com>
2022-04-16 16:28:55 +0430

    add try connect for publisher

--- rabbitmq/publisher.go
+++ rabbitmq/publisher.go
@@ -4,45 +4,52 @@ import (
 	"fmt"
 	"github.com/streadway/amqp"
 	"go.mongodb.org/mongo-driver/bson"
+	"time"
 )
 
 //Publish publishes a request to the amqp queue
-func (c *Connection) Publish(key, replayTo, contentType, correlationID string, priority uint8, body interface{}) error {
-	msg := message{RoutingKey: key, ReplyTo: replayTo, ContentType: contentType, CorrelationID: correlationID, Priority: priority}
+func (c *Connection) Publish(exchange, routingKey, replayTo, contentType, correlationID string, headers amqp.Table, body interface{}) error {
+	if !checkElementInSlice(c.exchanges, exchange) {
+		return EXHCNAGE_NOT_FOUND_ERROR
+	}
+	// serialized message to bson
 	b, err := bson.Marshal(body)
 	if err != nil {
 		return err
 	}
-	msg.Body = b
-	return publish(msg, c)
+	// try to publish event
+	if err := publish(exchange, routingKey, amqp.Publishing{
+		Headers:       headers,
+		ContentType:   contentType,
+		CorrelationId: correlationID,
+		ReplyTo:       replayTo,
+		Body:          b,
+	}, c); err != nil {
+		if err == CONNECTION_CLOSED_ERROR {
+			for {
+				if c.isConnected {
+					break
+				}
+				time.Sleep(1 * time.Second)
+			}
+		}
+		return err
+	}
+	return nil
 }
 
-//// Publish publishes the data argument to the push amqp message. The data argument
-//// will be encoded using the associated encoder.
-//func (c *EncodedConn) Publish(key, replayTo, contentType, correlationID string, priority uint8, body interface{}) error {
-//	msg := message{RoutingKey: key, ReplyTo: replayTo, ContentType: contentType, CorrelationID: correlationID, Priority: priority}
-//	encBody, err := c.Enc.Encode(body)
-//	if err != nil {
-//		return err
-//	}
-//	msg.Body = encBody
-//	return publish(msg, c.Conn)
-//}
-
-func publish(msg message, conn *Connection) error {
+func publish(exchange, routingKey string, msg amqp.Publishing, conn *Connection) error {
 	if !conn.isConnected {
 		return CONNECTION_CLOSED_ERROR
 	}
 	p := amqp.Publishing{
 		ContentType:   msg.ContentType,
-		CorrelationId: msg.CorrelationID,
+		CorrelationId: msg.CorrelationId,
 		Body:          msg.Body,
 		ReplyTo:       msg.ReplyTo,
 	}
-	for exchange, _ := range conn.exchanges {
-		if err := conn.channel.Publish(exchange, msg.RoutingKey, false, false, p); err != nil {
-			return fmt.Errorf("Error in Publishing: %s", err)
-		}
+	if err := conn.channel.Publish(exchange, routingKey, false, false, p); err != nil {
+		return fmt.Errorf("error in Publishing: %s", err)
 	}
 	return nil
 }
