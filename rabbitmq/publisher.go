636fe42ea543f360690195f8fe8ab717eb80a1a5
Javad <ja7ad@live.com>
2022-04-17 13:10:38 +0430

    update publisher and get publishingOptions for event

--- rabbitmq/publisher.go
+++ rabbitmq/publisher.go
@@ -2,13 +2,14 @@ package rabbitmq
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/streadway/amqp"
 	"go.mongodb.org/mongo-driver/bson"
 	"time"
 )
 
 //Publish publishes a request to the amqp queue
-func (c *Connection) Publish(exchange, routingKey, replayTo, contentType, correlationID string, headers amqp.Table, body interface{}) error {
+func (c *Connection) Publish(exchange, routingKey string, publishOptions PublishingOptions, body interface{}) error {
 	if !checkElementInSlice(c.exchanges, exchange) {
 		return EXHCNAGE_NOT_FOUND_ERROR
 	}
@@ -18,14 +19,8 @@ func (c *Connection) Publish(exchange, routingKey, replayTo, contentType, correl
 		return err
 	}
 	// try to publish event
-	if err := publish(exchange, routingKey, amqp.Publishing{
-		Headers:       headers,
-		ContentType:   contentType,
-		CorrelationId: correlationID,
-		ReplyTo:       replayTo,
-		Body:          b,
-	}, c); err != nil {
-		if err == CONNECTION_CLOSED_ERROR {
+	if err := publish(c, exchange, routingKey, publishOptions, b); err != nil {
+		if errors.Is(err, CONNECTION_CLOSED_ERROR) {
 			for {
 				if c.isConnected {
 					break
@@ -38,15 +33,24 @@ func (c *Connection) Publish(exchange, routingKey, replayTo, contentType, correl
 	return nil
 }
 
-func publish(exchange, routingKey string, msg amqp.Publishing, conn *Connection) error {
+func publish(conn *Connection, exchange, routingKey string, publishingOptions PublishingOptions, body []byte) error {
 	if !conn.isConnected {
 		return CONNECTION_CLOSED_ERROR
 	}
 	p := amqp.Publishing{
-		ContentType:   msg.ContentType,
-		CorrelationId: msg.CorrelationId,
-		Body:          msg.Body,
-		ReplyTo:       msg.ReplyTo,
+		Headers:         amqp.Table(publishingOptions.Headers),
+		ContentType:     publishingOptions.ContentType,
+		ContentEncoding: publishingOptions.ContentEncoding,
+		DeliveryMode:    publishingOptions.DeliveryMode,
+		Priority:        publishingOptions.Priority,
+		CorrelationId:   publishingOptions.CorrelationId,
+		ReplyTo:         publishingOptions.ReplyTo,
+		Expiration:      publishingOptions.Expiration,
+		MessageId:       publishingOptions.MessageId,
+		Timestamp:       publishingOptions.Timestamp,
+		UserId:          publishingOptions.UserId,
+		AppId:           publishingOptions.AppId,
+		Body:            body,
 	}
 	if err := conn.channel.Publish(exchange, routingKey, false, false, p); err != nil {
 		return fmt.Errorf("error in Publishing: %s", err)
